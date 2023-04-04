8b524a643ed9225cf547a89941a5064db1a32a01
Cali Castle <hi@cali.so>
2023-04-05 01:45:47 +0800

    fix: build error

--- app/app.d.ts
+++ app/app.d.ts
@@ -2,4 +2,5 @@ type Locale = 'en-US' | 'zh-CN'
 type RootParams = { locale: Locale }
 
 type Messages = typeof import('~/translations/en-US.json')
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 declare interface IntlMessages extends Messages {}

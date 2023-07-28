7d97607ff794dcdce5bf4b36da8ba8dad932f2c3
jsun969 <i@jsun.lol>
2023-07-28 13:06:20 +0800

    fix: admin auth middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,11 @@ import { authMiddleware, clerkClient, redirectToSignIn } from '@clerk/nextjs'
 export default authMiddleware({
   async afterAuth(auth, req) {
     if (req.nextUrl.pathname === '/admin') {
-      const user = await clerkClient.users.getUser(auth.userId!)
+      if (!auth.userId) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return redirectToSignIn({ returnBackUrl: req.url })
+      }
+      const user = await clerkClient.users.getUser(auth.userId)
       if (!user.publicMetadata?.isAdmin) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return redirectToSignIn({ returnBackUrl: req.url })

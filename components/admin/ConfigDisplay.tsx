24317e54d4e236c0bcaf43b84c193ac52ac8e863
jsun969 <i@jsun.lol>
2023-08-03 12:05:51 +0800

    feat(admin): add total questions count

--- components/admin/ConfigDisplay.tsx
+++ components/admin/ConfigDisplay.tsx
@@ -1,13 +1,17 @@
+import { sql } from 'drizzle-orm'
 import React from 'react'
 
 import { db } from '~/db'
-import { config } from '~/db/schema'
+import { config, questions } from '~/db/schema'
 
 export async function ConfigDisplay() {
   const [configData] = await db.select().from(config)
+  const questionCount =
+    (await db.select({ count: sql<number>`count(*)` }).from(questions))[0]
+      ?.count || 0
 
   return (
-    <div className="space-x-2 text-sm sm:flex">
+    <div className="space-x-2 text-center text-sm sm:flex">
       <div>
         <span className="font-bold">Release Date: </span>
         <span>{configData?.releaseDate?.toDateString()}</span>
@@ -19,6 +23,10 @@ export async function ConfigDisplay() {
       <div>
         <span className="font-bold">Questions Per Challenge: </span>
         <span>{configData?.questionsPerChallenge}</span>
+      </div>
+      <div>
+        <span className="font-bold">Total Questions: </span>
+        <span>{questionCount}</span>
       </div>
     </div>
   )

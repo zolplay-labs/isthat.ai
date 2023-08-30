c442c7ee1934547b20c2e060795594e67e8e25f8
jsun969 <i@jsun.lol>
2023-08-30 11:47:18 +0800

    feat(auth): remove auth page

--- app/(auth)/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from '@clerk/nextjs/app-beta'
-
-import { authTheme } from '~/app/(auth)/theme'
-
-export default function Page() {
-  return <SignIn signUpUrl="/signup" appearance={authTheme} />
-}
--- app/(auth)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUp } from '@clerk/nextjs/app-beta'
-
-import { authTheme } from '~/app/(auth)/theme'
-
-export default function Page() {
-  return <SignUp signInUrl="/signin" appearance={authTheme} />
-}

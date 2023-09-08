9eba662f32a09be5326060d310813ab07e81bfaf
jsun969 <i@jsun.lol>
2023-09-09 04:04:35 +0800

    feat(admin): remove auth url and add unauth page

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
-import { authMiddleware, clerkClient, redirectToSignIn } from '@clerk/nextjs'
+import { authMiddleware, clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export default authMiddleware({
   async afterAuth(auth, req) {
     if (req.nextUrl.pathname === '/admin') {
       if (!auth.userId) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return redirectToSignIn({ returnBackUrl: req.url })
+        return NextResponse.rewrite(new URL('/admin/unauthorized', req.url))
       }
       const user = await clerkClient.users.getUser(auth.userId)
       if (!user.publicMetadata?.isAdmin) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return redirectToSignIn({ returnBackUrl: req.url })
+        return NextResponse.rewrite(new URL('/admin/unauthorized', req.url))
       }
     }
   },

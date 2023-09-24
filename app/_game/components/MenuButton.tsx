eabc533f375b0ae28ea8873ab318645570004fd5
jsun969 <i@jsun.lol>
2023-09-24 22:05:29 +0800

    feat(game/style): add menu scene style

---
+++ app/_game/components/MenuButton.tsx
@@ -0,0 +1,19 @@
+interface MenuButtonProps {
+  children: string
+  onClick: () => void
+}
+
+export function MenuButton({ children, onClick }: MenuButtonProps) {
+  return (
+    <div className="group flex gap-[17px] text-center text-[14px] leading-[24px] sm:gap-[45px] sm:text-[32px] sm:leading-[48px]">
+      <div className="hidden select-none group-hover:block">&gt;</div>
+      <button
+        onClick={onClick}
+        className="block text-[#929292] group-hover:text-white"
+      >
+        {children}
+      </button>
+      <div className="hidden select-none group-hover:block">&lt;</div>
+    </div>
+  )
+}
--- app/_game/scenes/Menu.tsx
+++ app/_game/scenes/Menu.tsx
@@ -1,29 +1,56 @@
-import { Button } from '@tremor/react'
+import Image from 'next/image'
 
 import { useScene } from '~/stores/Scene.store'
 
+import { MenuButton } from '../components/MenuButton'
 import { useUser } from '../hooks/useUser'
 
 export function Menu() {
-  const { isSignedIn, logout, signInWithGoogle } = useUser()
+  const { isSignedIn, logout, signInWithGoogle, user } = useUser()
   const { switchScene } = useScene()
 
   return (
-    <>
-      <div>Isthat.ai</div>
-      {isSignedIn ? (
-        <>
-          <Button onClick={() => switchScene('WARM_UP')}>
-            Start challenge
-          </Button>
-          <Button onClick={logout}>Logout</Button>
-        </>
-      ) : (
-        <>
-          <Button onClick={signInWithGoogle}>Sign in with Google</Button>
-          <Button onClick={() => switchScene('WARM_UP')}>Trial Play</Button>
-        </>
+    <div className="relative flex h-[100dvh] items-center justify-center bg-[url('/images/menu/screen.svg')] bg-cover bg-center bg-no-repeat">
+      {isSignedIn && user?.avatar && (
+        <div className="absolute right-[5%] top-[16%] border-[3px] border-white sm:right-[20%]">
+          <div className="absolute left-[-3px] top-[-3px] h-[3px] w-[3px] bg-[#282828]" />
+          <div className="absolute right-[-3px] top-[-3px] h-[3px] w-[3px] bg-[#282828]" />
+          <div className="absolute bottom-[-3px] left-[-3px] h-[3px] w-[3px] bg-[#282828]" />
+          <div className="absolute bottom-[-3px] right-[-3px] h-[3px] w-[3px] bg-[#282828]" />
+          <Image src={user.avatar} alt="avatar" height={42} width={42} />
+        </div>
       )}
-    </>
+      <div className="flex flex-col items-center">
+        <Image
+          src="/images/logo.svg"
+          alt="logo"
+          className="h-[168px] w-[168px] sm:h-[200] sm:w-[200px]"
+          width={200}
+          height={200}
+        />
+        <div className="mb-[50px] text-[14px] leading-[normal] sm:mb-[100px] sm:text-[24px]">
+          Isthat.ai
+        </div>
+        <div className="flex flex-col items-center gap-[24px] sm:gap-[44px]">
+          {isSignedIn ? (
+            <>
+              <MenuButton onClick={() => switchScene('WARM_UP')}>
+                Start challenge
+              </MenuButton>
+              <MenuButton onClick={logout}>Logout</MenuButton>
+            </>
+          ) : (
+            <>
+              <MenuButton onClick={signInWithGoogle}>
+                Sign in with Google
+              </MenuButton>
+              <MenuButton onClick={() => switchScene('WARM_UP')}>
+                Trial Play
+              </MenuButton>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }

9e01e24f813430916b2dd17b947a6fe2e375bb00
jsun969 <i@jsun.lol>
2023-09-27 01:54:56 +0800

    style(game/loading): 🤡

--- app/_game/components/LoadingComputer.tsx
+++ app/_game/components/LoadingComputer.tsx
@@ -26,12 +26,12 @@ export function LoadingComputer({
           Isthat.ai
         </div>
       </div>
-      <div className="relative mb-[4px] mt-[12px] border-[1px] border-[#d9d9d9] p-[2px] sm:mb-[7px] sm:mt-[24px] sm:border-[2px] sm:p-[4px]">
+      <div className="relative mb-[4px] mt-[12px] border-[1px] border-[#D9D9D9] p-[2px] sm:mb-[7px] sm:mt-[24px] sm:border-[2px] sm:p-[4px]">
         <div className="absolute left-[-1px] top-[-1px] h-[1px] w-[1px] bg-[#282828] sm:left-[-2px] sm:top-[-2px] sm:h-[2px] sm:w-[2px]" />
         <div className="absolute right-[-1px] top-[-1px] h-[1px] w-[1px] bg-[#282828] sm:right-[-2px] sm:top-[-2px] sm:h-[2px] sm:w-[2px]" />
         <div className="absolute bottom-[-1px] left-[-1px] h-[1px] w-[1px] bg-[#282828] sm:bottom-[-2px] sm:left-[-2px] sm:h-[2px] sm:w-[2px]" />
         <div className="absolute bottom-[-1px] right-[-1px] h-[1px] w-[1px] bg-[#282828] sm:bottom-[-2px] sm:right-[-2px] sm:h-[2px] sm:w-[2px]" />
-        <div className="flex items-start gap-[2px] sm:gap-[4px]">
+        <div className="flex gap-[2px] sm:gap-[4px]">
           {new Array(10).fill(null).map((_, i) => (
             <div
               key={i}

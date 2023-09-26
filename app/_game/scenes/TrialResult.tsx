73bf68e45a32f71a739647a36bd518e8cbb7c347
jsun969 <i@jsun.lol>
2023-09-26 18:35:26 +0800

    feat(game/style): add trial result style

--- app/_game/scenes/TrialResult.tsx
+++ app/_game/scenes/TrialResult.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@tremor/react'
+import Image from 'next/image'
 
 import { useSceneProps } from '~/stores/SceneProps.store'
 
@@ -12,19 +12,54 @@ export function TrialResult() {
   const { signInWithGoogle } = useUser()
 
   return (
-    <GameLayout header={<span>~ Result ~</span>}>
-      <div>{props.isRight ? '😀' : '🙁'}</div>
-      <div>
-        {props.isRight
-          ? '~ Congratulations, you got it right! ~'
-          : "~ I'm sorry, you got it wrong. ~"}
+    <GameLayout
+      header={<span>~ Result ~</span>}
+      className="flex flex-col items-center justify-center text-center"
+    >
+      <Image
+        src={
+          props.isRight ? '/images/result/smile.svg' : '/images/result/sad.svg'
+        }
+        alt="Grade Image"
+        className="h-[168px] w-[168px] sm:h-[268px] sm:w-[268px]"
+        width={268}
+        height={268}
+      />
+      <div className="mt-[12px] text-[10px] sm:mt-[8px] sm:text-[16px]">
+        <div className="hidden sm:block">
+          {props.isRight
+            ? '~ Congratulations! You got it right! ~'
+            : "~ I'm sorry. You got it wrong. ~"}
+        </div>
+        <div className="sm:hidden">
+          {props.isRight ? (
+            <>
+              <div>Congratulations!</div>
+              <div>You got it right!</div>
+            </>
+          ) : (
+            <>
+              <div>I&apos;m sorry.</div>
+              <div>You got it wrong.</div>
+            </>
+          )}
+        </div>
       </div>
-      <div>
+      <div className="mb-[24px] mt-[44px] text-[8px] sm:mt-[58px] sm:text-[13px]">
         {props.isRight
           ? "Continuing the challenge won't be that easy..."
           : "Do you want to tell me that you're not just capable of this much?"}
       </div>
-      <Button onClick={signInWithGoogle}>Sign up and Battle with AI</Button>
+      <button
+        onClick={signInWithGoogle}
+        className="relative block border-[4px] border-white py-[6px] text-[9px] leading-[24px] sm:p-[6px] sm:text-[14px]"
+      >
+        <div className="absolute left-[-4px] top-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute right-[-4px] top-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute bottom-[-4px] left-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute bottom-[-4px] right-[-4px] h-[4px] w-[4px] bg-black" />
+        Sign up and Battle with AI
+      </button>
     </GameLayout>
   )
 }

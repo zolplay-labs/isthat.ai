8fe42dda187c178621dec58aeb28f4fb6e459784
jsun969 <i@jsun.lol>
2023-10-09 18:12:47 +0800

    feat(game/result): add share dialog

--- app/_game/scenes/Result.tsx
+++ app/_game/scenes/Result.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@tremor/react'
+import { motion, useDragControls } from 'framer-motion'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
+import { useIsPC } from '~/hooks/useIsPC'
 import { sqids } from '~/lib/sqids'
 import { useSceneProps } from '~/stores/SceneProps.store'
 
@@ -9,30 +10,101 @@ import { GameLayout } from '../components/GameLayout'
 import { ResultDisplay } from '../components/ResultDisplay'
 import { useUser } from '../hooks/useUser'
 
-// TODO: Add copy share link dialog style
-function CopyShareLinkDialog({
-  shareLink,
-  onClose,
+function ShareDialogButton({
+  children,
+  onClick,
 }: {
+  children: string
+  onClick: () => void
+}) {
+  return (
+    <button
+      className="relative block cursor-pointer border-[2px] border-white p-[10px] text-[15px]"
+      onClick={onClick}
+    >
+      <div className="absolute left-[-2px] top-[-2px] h-[2px] w-[2px] bg-black" />
+      <div className="absolute right-[-2px] top-[-2px] h-[2px] w-[2px] bg-black" />
+      <div className="absolute bottom-[-2px] left-[-2px] h-[2px] w-[2px] bg-black" />
+      <div className="absolute bottom-[-2px] right-[-2px] h-[2px] w-[2px] bg-black" />
+      {children}
+    </button>
+  )
+}
+
+interface ShareDialogProps {
   shareLink: string
   onClose: () => void
-}) {
-  const [copyButtonText, setCopyButtonText] = useState('Copy')
+  dragConstraintsRef: React.RefObject<HTMLDivElement>
+}
+
+function ShareDialog({
+  shareLink,
+  onClose,
+  dragConstraintsRef,
+}: ShareDialogProps) {
+  const dragControls = useDragControls()
+
+  const [isCopied, setIsCopied] = useState(false)
   const handleCopy = async () => {
     await navigator.clipboard.writeText(shareLink)
-    setCopyButtonText('Copied!')
+    setIsCopied(true)
   }
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false)
+      }, 1000)
+    }
+  }, [isCopied])
 
   return (
-    <div className="border-2 border-black p-2">
-      <Button className="cursor-pointer" onClick={onClose}>
-        Close
-      </Button>
-      <div>Share link: {shareLink}</div>
-      <Button className="cursor-pointer" onClick={handleCopy}>
-        {copyButtonText}
-      </Button>
-    </div>
+    <motion.div
+      className="fixed"
+      drag
+      dragConstraints={dragConstraintsRef}
+      dragControls={dragControls}
+      dragMomentum={false}
+      dragElastic={false}
+      dragListener={false}
+    >
+      <div className="relative border-[4px] border-white bg-[#5A5A5A] p-[12px]">
+        <div className="absolute left-[-4px] top-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute right-[-4px] top-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute bottom-[-4px] left-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="absolute bottom-[-4px] right-[-4px] h-[4px] w-[4px] bg-black" />
+        <div className="mb-[12px] flex justify-between selection:items-center">
+          <div className="text-[16px]">Share</div>
+          <div
+            className="flex-1 cursor-grab"
+            onPointerDown={(e) => {
+              dragControls.start(e)
+            }}
+          />
+          <button className="block cursor-pointer" onClick={onClose}>
+            <Image
+              src="/images/result/close.svg"
+              alt="close"
+              width={24}
+              height={24}
+            />
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-[20px] bg-black p-[20px]">
+          <div className="text-[12px] text-[#a9a9a9]">~ Share isthat.ai ~</div>
+          <ShareDialogButton onClick={handleCopy}>
+            {isCopied ? 'Copied!' : 'Copy link'}
+          </ShareDialogButton>
+          <ShareDialogButton
+            // TODO: Add save image action
+            onClick={() => {
+              return
+            }}
+          >
+            Save image
+          </ShareDialogButton>
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
@@ -45,9 +117,11 @@ export function Result() {
     total: sceneProps['PLAY'].total,
   }
 
-  const [shareLinkForCopy, setShareLinkForCopy] = useState('')
-  const [isCopyShareLinkDialogOpen, setIsCopyShareLinkDialogOpen] =
-    useState(false)
+  const isPC = useIsPC()
+  const [shareLink, setShareLink] = useState('')
+  const [isCopyShareLinkDialogOpen, setIsShareDialogOpen] = useState(false)
+  const dialogDragConstraintsRef = useRef<HTMLDivElement>(null)
+
   const handleShare = async () => {
     const shareId = sqids.encode([scoreId])
     const shareUrl = `/share/${shareId}`
@@ -61,9 +135,9 @@ export function Result() {
       navigator.share(shareData)
       return
     }
-    if (navigator.clipboard) {
-      setShareLinkForCopy(window.location.origin + shareUrl)
-      setIsCopyShareLinkDialogOpen(true)
+    if (navigator.clipboard && isPC) {
+      setShareLink(window.location.origin + shareUrl)
+      setIsShareDialogOpen(true)
       return
     }
     alert('Unable to share!')
@@ -73,6 +147,7 @@ export function Result() {
     <GameLayout
       header={<span>~ Result ~</span>}
       className="relative flex h-full w-full flex-col items-center justify-center gap-[35px] px-[10px] sm:px-[30px]"
+      ref={dialogDragConstraintsRef}
     >
       <button
         className="absolute right-[10px] top-[10px] flex cursor-pointer items-center gap-[4px] sm:right-[30px] sm:top-[30px] sm:gap-[8px]"
@@ -94,9 +169,10 @@ export function Result() {
         date={new Date()}
       />
       {isCopyShareLinkDialogOpen && (
-        <CopyShareLinkDialog
-          onClose={() => setIsCopyShareLinkDialogOpen(false)}
-          shareLink={shareLinkForCopy}
+        <ShareDialog
+          onClose={() => setIsShareDialogOpen(false)}
+          shareLink={shareLink}
+          dragConstraintsRef={dialogDragConstraintsRef}
         />
       )}
     </GameLayout>

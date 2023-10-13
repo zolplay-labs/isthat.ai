6784a7cf30776632c34b7cbe1f7a0e385335bfb2
Ethan <ethan@zolplay.com>
2023-10-13 11:33:20 -0500

    feat(metadata): add metadata (#28)
    * feat(metadata): add metadata
    
    * feat(metadata): update social card
    
    * feat(metadata): Add ResultTier to social card
    
    * fix(utils): rm formatLocaleDate
    
    * fix(social-card): rm edge runtime configure
    
    * fix(env): add PAGE_HOST to env.mjs
    
    * feat(env): `PAGE_HOST` -> `HOSTNAME`
    
    * fix(metadata): correct font name
    
    * refactor(social-card): Using Tailwind CSS - Experimental
    
    ---------
    
    Co-authored-by: jsun969 <i@jsun.lol>

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,43 @@ import { clsxm } from '@zolplay/utils'
 import { type Metadata } from 'next'
 import { Press_Start_2P } from 'next/font/google'
 
+const title = 'isthat.ai'
+const description = 'A.I. or not? The image Turing test for humans'
+
 export const metadata: Metadata = {
-  title: 'Is That AI',
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: 'https://isthat.ai',
+    siteName: title,
+    images: [
+      {
+        url: '/images/social/og.png',
+        width: 1200,
+        height: 675,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    site: '@zolplay',
+    creator: '@zolplay',
+    images: ['/images/social/og.png'],
+  },
+  viewport: {
+    height: 'device-height',
+    width: 'device-width',
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 }
 
 const pressStart2P = Press_Start_2P({
---
+++ app/share/[id]/opengraph-image.tsx
@@ -0,0 +1,6 @@
+import Image from './image'
+
+export const alt = 'isthat.ai'
+export const contentType = 'image/png'
+
+export default Image

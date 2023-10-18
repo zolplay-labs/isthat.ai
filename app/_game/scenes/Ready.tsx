4ad89a2900fdcc3b0c41baf1b5bc266be9b77639
jsun969 <i@jsun.lol>
2023-10-18 11:42:10 +0800

    feat(game/ready): 😋

--- app/_game/scenes/Ready.tsx
+++ app/_game/scenes/Ready.tsx
@@ -6,6 +6,18 @@ import { useScene } from '~/stores/Scene.store'
 
 import { GameLayout } from '../components/GameLayout'
 
+function Header() {
+  return (
+    <span>
+      <span>Ready to show </span>
+      <span className="bg-[linear-gradient(180deg,#E3FFB5_0%,#9E95FF_60.09%,#0E7FA3_100%,#712E91_100%,#D57DFF_100%)] bg-clip-text text-transparent">
+        AI
+      </span>
+      <span> who&apos;s the boss?</span>
+    </span>
+  )
+}
+
 export function Ready() {
   const { switchScene } = useScene()
 
@@ -25,10 +37,7 @@ export function Ready() {
   }, [countdown])
 
   return (
-    <GameLayout
-      header={<span>Ready to show A.I. who&apos;s the boss?</span>}
-      className="text-[120px] sm:text-[240px]"
-    >
+    <GameLayout header={<Header />} className="text-[120px] sm:text-[240px]">
       {countdown === 0 ? 'GO' : countdown}
     </GameLayout>
   )

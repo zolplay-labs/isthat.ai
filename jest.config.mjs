6bbbec9bed478e102b7c018d485aabfe7339c78b
jsun969 <i@jsun.lol>
2023-10-18 22:00:42 +0800

    chore(jest): add timezone

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,7 @@
 import nextJest from 'next/jest.js'
 
+process.env.TZ = 'GMT'
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',

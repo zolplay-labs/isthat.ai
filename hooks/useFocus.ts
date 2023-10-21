ef0b87e16b840483a4340de3f2d94ab81cdac86f
jsun969 <i@jsun.lol>
2023-10-21 20:40:39 +0800

    fix(hook): add `onFocus` in `useFocus` dependencies

--- hooks/useFocus.ts
+++ hooks/useFocus.ts
@@ -1,13 +1,10 @@
 import { useEffect } from 'react'
 
 export const useFocus = (onFocus: () => void, deps?: React.DependencyList) => {
-  useEffect(
-    () => {
-      window.addEventListener('focus', onFocus)
-      return () => {
-        window.removeEventListener('focus', onFocus)
-      }
-    },
-    deps?.length ? deps : []
-  )
+  useEffect(() => {
+    window.addEventListener('focus', onFocus)
+    return () => {
+      window.removeEventListener('focus', onFocus)
+    }
+  }, [onFocus, ...(deps || [])])
 }

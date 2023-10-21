c63059e00ccc6590cf85366677e3f0ff74d2d51f
jsun969 <i@jsun.lol>
2023-10-21 13:56:31 +0800

    fix: `shuffleArray`

--- utils/shuffleArray.ts
+++ utils/shuffleArray.ts
@@ -2,7 +2,7 @@ export const shuffleArray = <T>(array: T[]): T[] => {
   const shuffledArray = [...array]
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const randomIndex = Math.floor(Math.random() * (i + 1))
-    ;[shuffledArray[i]!, shuffledArray[randomIndex]!] = [
+    ;[shuffledArray[i], shuffledArray[randomIndex]] = [
       shuffledArray[randomIndex]!,
       shuffledArray[i]!,
     ]

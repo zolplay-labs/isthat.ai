6c0e4c8a80e89273536043cdf403180e8ed7e869
Cali Castle <hi@cali.so>
2023-10-21 23:53:34 +0800

    fix: share page should bypass clerk

--- lib/url.ts
+++ lib/url.ts
@@ -2,7 +2,7 @@ export function url(path = '') {
   const baseUrl =
     process.env.VERCEL_ENV === 'production'
       ? 'https://isthat.ai'
-      : 'http://beta.isthat.ai'
+      : 'https://beta.isthat.ai'
 
   return new URL(path, baseUrl)
 }

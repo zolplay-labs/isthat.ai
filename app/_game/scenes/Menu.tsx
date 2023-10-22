d1468ffaa0bec64735337d44821a2b130015eed5
jsun969 <i@jsun.lol>
2023-10-22 23:48:43 +0800

    feat: remove preload game images

--- app/_game/scenes/Menu.tsx
+++ app/_game/scenes/Menu.tsx
@@ -11,7 +11,6 @@ import { useSceneProps } from '~/stores/SceneProps.store'
 
 import { BorderWithoutCorner } from '../components/BorderWithoutCorner'
 import { MenuButton } from '../components/MenuButton'
-import { PreloadImages } from '../components/PreloadImages'
 import { useUser } from '../hooks/useUser'
 
 const formatTime = (seconds: number) => {
@@ -55,7 +54,6 @@ export function Menu() {
 
   return (
     <div className="relative flex h-[100dvh] items-center justify-center bg-[url('/images/menu/screen.svg')] bg-cover bg-center bg-no-repeat">
-      <PreloadImages />
       <div className="absolute top-[16%] flex w-[80%] items-center justify-between sm:w-[60%]">
         <a
           className="flex cursor-click items-baseline gap-[8px] sm:gap-[12px]"
